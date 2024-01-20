8149139bdaa7a9f74adbfce7446d494aa53673c2
Lytton <lytton.liao@gmail.com>
2024-01-20 00:01:32 -0500

    Waiting for brokerage account to be validated

--- run_robot.py
+++ run_robot.py
@@ -23,5 +23,31 @@ trading_robot = PyRobot(
     client_id=CLIENT_ID,
     redirect_uri=REDIRECT_URI,
     credentials_path=CREDENTIALS_PATH,
-    trading_account=ACCOUNT_NUMBER
+    trading_account=ACCOUNT_NUMBER,
+    paper_trading=True
 )
+
+# Create a new portfolio.
+trading_robot_portfolio = trading_robot.create_portfolio()
+
+# Add multiple positions to our portfolio.
+multi_position = [
+    {
+        'asset_type': 'equity',
+        'quantity': 2,
+        'purchase_price': 4.00,
+        'symbol': 'TSLA',
+        'purchase_date': '2024-01-19'
+    },
+    {
+        'asset_type': 'equity',
+        'quantity': 2,
+        'purchase_price': 4.00,
+        'symbol': 'SQ',
+        'purchase_date': '2024-01-19'
+    },
+]
+
+# Add those positions to the portfolio.
+new_positions = trading_robot.portfolio.add_positions(positions=multi_position)
+pprint.pprint(new_positions)
